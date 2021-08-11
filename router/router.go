acf2a542a011e1cd46382190e78465bbe159733e
haojie06 <haojie06@qq.com>
2021-08-11 23:49:32 +0800

    加入mdui

--- router/router.go
+++ router/router.go
@@ -9,13 +9,21 @@ import (
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-	// 添加 Get 请求路由
+	router.Static("/assets","./assets")
+	router.LoadHTMLGlob("template/**/*")
+	// TODO index之后做成主页 未登录重定向至登录页
 	router.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "hello gin")
+		context.HTML(http.StatusOK, "home/index.tmpl",gin.H{
+			"title": "Hello Gin",
+		})
 	})
-
 	//router.GET("/file",handler.GetFileList())
 	router.GET("/fs/*path", handler.GetFileHandler)
+	// 打开编辑器的前端页面，通过api获取文章内容(文章路径通过参数给出 )
+	// 前端相关的路由
+	//editorRouter := router.Group("/editor")
+	{
+		//editorRouter.GET()
+	}
 	return router
 }
