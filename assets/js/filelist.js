725c3a508e93de6bc462fdb971ede99c00211cbb
haojie06 <haojie06@qq.com>
2021-08-24 11:19:50 +0800

    改用扩展面板显示文章

--- assets/js/filelist.js
+++ assets/js/filelist.js
@@ -7,7 +7,20 @@ function getFileList(type, catetgory, page, size) {
                 let fileList = ""
                 data.files.forEach((file) => {
                     console.log(file)
-                    fileList += `<li class="mdui-list-item mdui-ripple"><div class="mdui-list-item-content">${file.FileName}</div></li>`
+                    fileList += `<div class="mdui-panel-item">
+                                    <div class="mdui-panel-item-header">
+                                        <div class="mdui-panel-item-title">${file.FileName}</div>
+                                        <div class="mdui-panel-item-summary">${file.FileModTime}</div>
+                                        <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+                                    </div>
+                                    <div class="mdui-panel-item-body">
+                                    <p>content</p>
+                                    <div class="mdui-panel-item-actions">
+                                        <button class="mdui-btn mdui-ripple" mdui-panel-item-close>取消</button>
+                                        <button class="mdui-btn mdui-ripple">编辑</button>
+                                    </div>
+                                    </div>
+                                 </div>`
                 })
                 mdui.$("#filelist").append(fileList)
             })
